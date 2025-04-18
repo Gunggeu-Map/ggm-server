36534f67dc9d8746de507668ecccec897bf7de01
ki_mimang <ki_mimang@ki-mimangui-MacBookAir.local>
2025-04-19 02:17:34 +0900

    feat: answer 레포지토리 생성

---
+++ src/main/java/com/gunggeumap/ggm/answer/repository/AnswerRepository.java
@@ -0,0 +1,12 @@
+package com.gunggeumap.ggm.answer.repository;
+
+import com.gunggeumap.ggm.answer.entity.Answer;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer,Long> {
+
+  List<Answer> findAnswerByQuestionId(Long questionId);
+
+
+}
