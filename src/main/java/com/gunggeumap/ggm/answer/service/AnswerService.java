0880ed9ec4e564812a8851ac94d2fa718d798e0f
ljg0610 <jggy0627@naver.com>
2025-04-20 01:38:46 +0900

    Feature/답변vote (#28)
    * feat: 답변 투표 타입 변경 메서드 추가
    
    * feat: 답변에대한 투표 서비스메서드 추가
    
    * feat: 답변에 대한 투표 컨트롤러 메서드 추가
    
    * feat: 로그인한 사용자 어떤 투표를 눌렀는지 확인할수있는 dto필드 추가
    
    * refactor: 답변조회 시 사용자의 투표 유무도 전달토록 수정

--- src/main/java/com/gunggeumap/ggm/answer/service/AnswerService.java
+++ src/main/java/com/gunggeumap/ggm/answer/service/AnswerService.java
@@ -2,12 +2,16 @@ package com.gunggeumap.ggm.answer.service;
 
 import com.gunggeumap.ggm.answer.dto.request.AnswerCreateRequest;
 import com.gunggeumap.ggm.answer.dto.response.AnswerResponse;
+import com.gunggeumap.ggm.answer.dto.response.VoteResponse;
 import com.gunggeumap.ggm.answer.entity.Answer;
+import com.gunggeumap.ggm.answer.enums.VoteType;
 import java.util.List;
 
 public interface AnswerService {
 
-  List<AnswerResponse> getAnswerByQuestionId(Long questionId);
+  List<AnswerResponse> getAnswerByQuestionId(Long questionId, Long userId);
 
   Void createAnswer(AnswerCreateRequest request,Long questionId);
+
+  VoteResponse voteAnswer(Long answerId, Long userId, VoteType voteType);
 }
