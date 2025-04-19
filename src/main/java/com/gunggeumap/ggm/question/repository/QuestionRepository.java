b4d6b40eb2161a0133cfa3bec2e0c029ff068adc
Seonghoon Jeong <105824163+wjdtjdgns@users.noreply.github.com>
2025-04-19 18:25:16 +0900

    feat: 지도 질문 api 구현 (#22)
    * feat: 지도 기반 질문 요약 api 구현
    
    * feat: 키워드 검색 질문 요약 리스트 api 구현
    
    * feat: 키워드 검색 질문 요약 리스트 api 구현

--- src/main/java/com/gunggeumap/ggm/question/repository/QuestionRepository.java
+++ src/main/java/com/gunggeumap/ggm/question/repository/QuestionRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
@@ -17,4 +18,24 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
   List<Question> findTopWithAnswers(Pageable pageable);
 
   Optional<Question> findQuestionById(Long id);
+
+  @Query("""
+    SELECT DISTINCT q FROM Question q
+    LEFT JOIN FETCH q.answers
+    WHERE within(q.location, ST_MakeEnvelope(:swLng, :swLat, :neLng, :neLat, 4326)) = true
+    """)
+  List<Question> findByMapBounds(
+      @Param("swLat") double swLat,
+      @Param("swLng") double swLng,
+      @Param("neLat") double neLat,
+      @Param("neLng") double neLng
+  );
+
+  @Query("""
+        SELECT DISTINCT q FROM Question q
+        LEFT JOIN FETCH q.answers
+        WHERE q.title ILIKE %:keyword% OR q.content ILIKE %:keyword%
+      """)
+  List<Question> findByKeywordContainingIgnoreCase(@Param("keyword") String keyword);
+
 }
